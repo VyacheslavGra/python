45a24ece9d607ff0cbcafe4fc1813182fd0699c0
Slavik <sapelkin.00@yandex.ru>
2023-12-04 00:30:28 +0300

    +

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -6,3 +6,12 @@ def sum_number():
     sum_numbers = number_1 + number_2
 
     print(f"{sum_numbers}")
+
+
+def dif_number():
+    number_1 = int(input())
+    number_2 = int(input())
+
+    dif_numbers = number_1 + number_2
+
+    print(f"{dif_numbers}")
