15889ec3ac3b9556b663a24cd21b28dd0322a58d
Slavik <sapelkin.00@yandex.ru>
2023-12-04 00:52:18 +0300

    -

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -1,16 +1,16 @@
 
 def sum_number():
-    number_1 = int(input())
-    number_2 = int(input())
+    number_1 = int(input('Введите число'))
+    number_2 = int(input('Введите число'))
 
     sum_numbers = number_1 + number_2
 
     print(f"{sum_numbers}")
 
 def dif_number():
-    number_1 = int(input())
-    number_2 = int(input())
+    number_1 = int(input('Введите число'))
+    number_2 = int(input('Введите число'))
 
-    dif_numbers = number_1 + number_2
+    dif_numbers = number_1 - number_2
 
     print(f"{dif_numbers}")
